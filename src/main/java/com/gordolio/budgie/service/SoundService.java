a46a52dfdb9cf5468f9fcc01c2c45858296d3292
Gordon Child <gordon.child@ihg.com>
2017-01-13 22:43:36 -0700

    fix build process, update resource loading

--- src/main/java/com/gordolio/budgie/service/SoundService.java
+++ src/main/java/com/gordolio/budgie/service/SoundService.java
@@ -1,11 +1,9 @@
 package com.gordolio.budgie.service;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,39 +16,42 @@ import javax.sound.sampled.Clip;
 import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class SoundService {
+public class SoundService implements InitializingBean {
 
     private static final Logger LOG = LoggerFactory.getLogger(SoundService.class);
 
     public static final List<String> FILE_NAMES = new ArrayList<>();
     private static final Map<String,byte[]> SOUNDS = new HashMap<>();
-    static {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    public void loadResources() {
         try {
-            getResourceFiles("/sounds/")
-                    .stream()
-                    .map(v->v.replace(".wav",""))
-                    .forEach(f->{
-                        try {
-                            FILE_NAMES.add(f);
-                            ByteArrayOutputStream output = new ByteArrayOutputStream();
-                            InputStream inputStream = SoundService.class.getResourceAsStream("/sounds/" + f + ".wav");
-                            byte[] buffer = new byte[2048];
-                            int bytesRead;
-                            while (-1 != (bytesRead = inputStream.read(buffer))) {
-                                output.write(buffer, 0, bytesRead);
-                            }
-                            SOUNDS.put(f, output.toByteArray());
-                            inputStream.close();
-                        } catch (Exception ex) {
-                            LOG.error("Could not read sound files", ex);
-                        }
-                    });
-        }catch(Exception ex) {
-            LOG.error("could not load files", ex);
+            Resource[] resources = this.applicationContext.getResources("classpath*:/sounds/*.wav");
+            for(Resource resource : resources) {
+                String f = resource.getFilename().replace(".wav","");
+                FILE_NAMES.add(f);
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                InputStream inputStream = resource.getInputStream();
+                byte[] buffer = new byte[2048];
+                int bytesRead;
+                while (-1 != (bytesRead = inputStream.read(buffer))) {
+                    output.write(buffer, 0, bytesRead);
+                }
+                SOUNDS.put(f, output.toByteArray());
+                inputStream.close();
+            }
+        } catch (IOException ex) {
+            LOG.error("Error reading resources", ex);
         }
     }
 
@@ -74,26 +75,8 @@ public class SoundService {
         }).start();
     }
 
-    private static List<String> getResourceFiles(String path) throws IOException {
-        List<String> filenames = new ArrayList<>();
-
-        try(InputStream in = getResourceAsStream(path);
-            BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            String resource;
-            while((resource = br.readLine()) != null) {
-                filenames.add(resource);
-            }
-        }
-        return filenames;
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.loadResources();
     }
-
-    private static InputStream getResourceAsStream(String resource ) {
-        final InputStream in = getContextClassLoader().getResourceAsStream(resource);
-        return in == null ? SoundService.class.getResourceAsStream(resource) : in;
-    }
-
-    private static ClassLoader getContextClassLoader() {
-        return Thread.currentThread().getContextClassLoader();
-    }
-
 }
